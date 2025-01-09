ed105e1469e267b727784d950e3baf726711a780
wailinnaing432019 <wailinnaing432019@gmail.com>
2025-01-09 15:14:04 +0630

    delete example

---
+++ delete.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma=new PrismaClient();
+
+prisma.post.deleteMany({
+    where:{userId:1},
+})
+.then(()=>{
+    prisma.user.delete({
+        where:{id:1},
+    })
+    .then(()=>{
+        console.log("Deleted user 1 and their posts");
+    }) 
+})
+.catch(e=>{
+    console.error(e);
+    process.exit(1);
+})
+.finally(()=>{
+    prisma.$disconnect();
+});
