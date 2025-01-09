6acd39deb0e222e9a7b09cfaa1a01249f9d252cf
wailinnaing432019 <wailinnaing432019@gmail.com>
2025-01-09 14:55:43 +0630

    read example

---
+++ read.js
@@ -0,0 +1,15 @@
+const {   PrismaClient } = require("@prisma/client");
+
+const prisma=new PrismaClient();
+prisma.user.findFirst({
+    where:{id:1},
+    include:{posts:true},
+})
+.then(data=>console.log(data))
+.catch(e=>{
+    console.error(e);
+    process.exit(1);
+})
+.finally(()=>{
+    prisma.$disconnect();
+})
